c11ce0b71a7433e1ad06344631b5b0927eb6edaa
Glitch (glitch-hello-eleventy) <none>
2022-02-13 22:27:29 +0000

    💪🐫 Checkpoint ./.eleventy.js:42179883/416

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,7 @@
 const pluginSEO = require("eleventy-plugin-seo");
 const { createHash } = require("crypto");
 
-/**
- * This is the JavaScript code that determines the config for your Eleventy site
- *
- * You can add lost of customization here to define how the site builds your content
- * Try extending it to suit your needs!
- */
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setTemplateFormats([
-    // Templates:
-    "html",
-    "njk",
-    "md",
-    // Static Assets:
-    "css",
-    "jpeg",
-    "jpg",
-    "png",
-    "svg",
-    "woff",
-    "woff2",
-  ]);
   eleventyConfig.addPassthroughCopy("public");
 
   eleventyConfig.addPlugin(pluginSEO, require("./src/_data/metadata.json"));
