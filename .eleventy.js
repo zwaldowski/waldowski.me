59396b1d94683c58fbeedd21a1c1b903c6aea2ce
Glitch (glitch-hello-eleventy) <none>
2022-02-13 20:50:05 +0000

    💽⚓️ Checkpoint ./package.json:42179883/1218 ./src/seo.json:42179883/603 ./src/index.md:42179883/360 ./.eleventy.js:42179883/13187 ./src/_includes/layouts/base.njk:42179883/1263 ./.glitch-assets:42179883/855 ./src/_data/metadata.json:42179883/704 ./src/_data/config.json:42179883/2 ./src/_includes/posts.home.njk:42179883/1017 ./src/_includes/components/posts.home.njk:42179883/16

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
-const { DateTime } = require("luxon");
 const pluginSEO = require("eleventy-plugin-seo");
+const { createHash } = require("crypto");
 
 /**
-* This is the JavaScript code that determines the config for your Eleventy site
-*
-* You can add lost of customization here to define how the site builds your content
-* Try extending it to suit your needs!
-*/
+ * This is the JavaScript code that determines the config for your Eleventy site
+ *
+ * You can add lost of customization here to define how the site builds your content
+ * Try extending it to suit your needs!
+ */
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     // Templates:
     "html",
@@ -21,25 +21,23 @@ module.exports = function(eleventyConfig) {
     "png",
     "svg",
     "woff",
-    "woff2"
+    "woff2",
   ]);
   eleventyConfig.addPassthroughCopy("public");
 
-  /* From: https://github.com/artstorm/eleventy-plugin-seo
-  
-  Adds SEO settings to the top of all pages
-  The "glitch-default" bit allows someone to set the url in seo.json while
-  still letting it have a proper glitch.me address via PROJECT_DOMAIN
-  */
-  const seo = require("./src/seo.json");
-  if (seo.url === "glitch-default") {
-    seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-  }
-  eleventyConfig.addPlugin(pluginSEO, seo);
+  eleventyConfig.addPlugin(pluginSEO, require("./src/_data/metadata.json"));
 
   // Filters let you modify the content https://www.11ty.dev/docs/filters/
-  eleventyConfig.addFilter("htmlDateString", dateObj => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  eleventyConfig.addFilter("htmlDateString", function (date) {
+    return date.toLocaleDateString();
+  });
+
+  eleventyConfig.addFilter("gravatar", function (email, size) {
+    const hash = createHash("md5")
+      .update(email.trim().toLowerCase())
+      .digest("hex");
+    const search = size ? `?s=${size}` : "";
+    return `https://www.gravatar.com/avatar/${hash}${search}`;
   });
 
   eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
@@ -47,18 +45,16 @@ module.exports = function(eleventyConfig) {
   /* Build the collection of posts to list in the site
      - Read the Next Steps post to learn how to extend this
   */
-  eleventyConfig.addCollection("posts", function(collection) {
-    
+  eleventyConfig.addCollection("posts", function (collection) {
     /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
        - https://www.11ty.dev/docs/collections/
     */
-    
+
     // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
     // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
-    const coll = collection
-      .getFilteredByTag("posts");
+    const coll = collection.getFilteredByTag("posts");
 
-    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
+    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
     // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
     for (let i = 0; i < coll.length; i++) {
       const prevPost = coll[i - 1];
@@ -75,7 +71,7 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "build"
-    }
+      output: "build",
+    },
   };
 };
