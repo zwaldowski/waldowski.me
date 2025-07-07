920bc8ea6729a7f03f4e4e5711e846d713d9fdee
Zachary Waldowski <zach@waldowski.me>
2025-07-06 20:41:32 -0700

    Theme background under page + SCSS

--- config/minify.js
+++ config/minify.js
@@ -1,23 +1,40 @@
 import { minify } from "html-minifier-terser"
-import { bundle } from "lightningcss"
+import { transform } from "lightningcss"
+import { dirname } from "node:path"
+import { compileStringAsync } from "sass"
 
 export default (eleventyConfig) => {
   const isProduction = process.env.NODE_ENV === "production"
+  const compileCSS = (css, filename) => () => transform({
+    filename,
+    code: Buffer.from(css),
+    minify: isProduction,
+  }).code
 
+  // Process CSS with LightningCSS
   eleventyConfig.addTemplateFormats("css")
   eleventyConfig.addExtension("css", {
     outputFileExtension: "css",
-    compile: (content, filename) => () => {
-      const result = bundle({
-        filename,
-        code: Buffer.from(content),
-        minify: isProduction,
+    compile: compileCSS
+  })
+
+  // Compile Sass and process with LightningCSS
+  eleventyConfig.addTemplateFormats("scss")
+  eleventyConfig.ignores.add("**/_*.scss")
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function (content, filename) {
+      const output = await compileStringAsync(content, {
+        loadPaths: [dirname(filename)],
       })
 
-      return result.code.toString()
+      this.addDependencies(filename, output.loadedUrls)
+
+      return compileCSS(output.css, filename)
     },
   })
 
+  // Minify HTML
   eleventyConfig.addTransform("htmlmin", async (content, filename) => {
     if (!isProduction || !filename.endsWith(".html")) {
       return content
